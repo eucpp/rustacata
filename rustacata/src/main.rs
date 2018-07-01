95ad9db505a8c91fd381227479af46002d754679
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2018-07-01 18:06:15 +0300

    iter mut example

--- rustacata/src/main.rs
+++ rustacata/src/main.rs
@@ -66,6 +66,56 @@ impl<'a, 'b, I, S> Transformer<&'b Expr, I, S> for ExprFold<'a, I, S> {
     }
 }
 
+struct ExprFoldMut<'a, I, S> {
+    fold_value : Box<'a + for<'b> Fn(&Transformer<&'b mut Expr, I, S>, I, &'b mut i32) -> S>,
+    fold_add : Box<'a + for <'b> Fn(&Transformer<&'b mut Expr, I, S>, I, &'b mut Box<Expr>, &'b mut Box<Expr>) -> S>,
+    fold_mult : Box<'a + for<'b> Fn(&Transformer<&'b mut Expr, I, S>, I, &'b mut Box<Expr>, &'b mut Box<Expr>) -> S>,
+}
+
+impl<'a, I, S> ExprFoldMut<'a, I, S> {
+
+    fn with_fold_value<'c: 'a, F>(self, f: F) -> Self
+    where
+        F: 'c + for<'b> Fn(&Transformer<&'b mut Expr, I, S>, I, &'b mut i32) -> S
+    {
+        ExprFoldMut { fold_value: Box::new(f), ..self }
+    }
+
+    fn with_fold_add<'c: 'a, F>(self, f: F) -> Self
+        where
+            F: 'c + for<'b> Fn(&Transformer<&'b mut Expr, I, S>, I, &'b mut Box<Expr>, &'b mut Box<Expr>) -> S
+    {
+        ExprFoldMut { fold_add: Box::new(f), ..self }
+    }
+
+    fn with_fold_mult<'c: 'a, F>(self, f: F) -> Self
+        where
+            F: 'c + for<'b> Fn(&Transformer<&'b mut Expr, I, S>, I, &'b mut Box<Expr>, &'b mut Box<Expr>) -> S
+    {
+        ExprFoldMut { fold_mult: Box::new(f), ..self }
+    }
+}
+
+impl<'a, I, S> Default for ExprFoldMut<'a, I, S> {
+    fn default() -> Self {
+        ExprFoldMut {
+            fold_value: Box::new(|tr, inh, v| unimplemented!()),
+            fold_add: Box::new(|tr, inh, e1, e2| unimplemented!()),
+            fold_mult: Box::new(|tr, inh, e1, e2| unimplemented!()),
+        }
+    }
+}
+
+impl<'a, 'b, I, S> Transformer<&'b mut Expr, I, S> for ExprFoldMut<'a, I, S> {
+    fn transform(&self, inh: I, x: &'b mut Expr) -> S {
+        match x {
+            Expr::Value(ref mut v) => (self.fold_value)(&*self, inh, v),
+            Expr::Add(ref mut e1, ref mut e2) => (self.fold_add)(&*self, inh, e1, e2),
+            Expr::Mult(ref mut e1, ref mut e2) => (self.fold_mult)(&*self, inh, e1, e2),
+        }
+    }
+}
+
 struct Evaluator<'a>(ExprFold<'a, (), i32>);
 
 impl<'a> Evaluator<'a> {
@@ -140,9 +190,56 @@ impl<'a, I> Map<'a, I> {
     }
 }
 
+struct IterMut<'a, I>(ExprFoldMut<'a, I, ()>);
+
+impl<'a, I: Copy> Default for IterMut<'a, I> {
+    fn default() -> Self {
+        IterMut(ExprFoldMut::default()
+            .with_fold_value(|tr, inh, v| {
+                ()
+            })
+            .with_fold_add(|tr, inh, e1, e2| {
+                tr.transform(inh, &mut **e1);
+                tr.transform(inh, &mut **e2);
+            })
+            .with_fold_mult(|tr, inh, e1, e2| {
+                tr.transform(inh, &mut **e1);
+                tr.transform(inh, &mut **e2);
+            })
+        )
+    }
+}
+
+impl<'a, I> IterMut<'a, I> {
+    fn with_iter_value<'c: 'a, F>(self, f: F) -> Self
+        where
+            F: 'c + for<'b> Fn(&Transformer<&'b mut Expr, I, ()>, I, &'b mut i32) -> ()
+    {
+        IterMut(ExprFoldMut { fold_value: Box::new(f), ..self.0 })
+    }
+
+    fn with_iter_add<'c: 'a, F>(self, f: F) -> Self
+        where
+            F: 'c + for<'b> Fn(&Transformer<&'b mut Expr, I, ()>, I, &'b mut Box<Expr>, &'b mut Box<Expr>) -> ()
+    {
+        IterMut(ExprFoldMut { fold_add: Box::new(f), ..self.0 })
+    }
+
+    fn with_iter_mult<'c: 'a, F>(self, f: F) -> Self
+        where
+            F: 'c + for<'b> Fn(&Transformer<&'b mut Expr, I, ()>, I, &'b mut Box<Expr>, &'b mut Box<Expr>) -> ()
+    {
+        IterMut(ExprFoldMut { fold_mult: Box::new(f), ..self.0 })
+    }
+
+    fn iter(&self, inh: I, e: &mut Expr) -> () {
+        self.0.transform(inh, e)
+    }
+}
+
 fn main() {
 //    // 2 * (7 + 1) = 16
-    let e = Expr::Mult(
+    let mut e = Expr::Mult(
         Box::new(Expr::Value(2)),
         Box::new(Expr::Add(
             Box::new(Expr::Value(7)),
@@ -161,5 +258,13 @@ fn main() {
         });
     let e_inc = inc_mapper.map((), &e);
     let v_inc = evaluator.eval(&e_inc);
+    println!("result={}", v_inc);
+
+    let inc_iter = IterMut::default().with_iter_value(
+        |tr, inh, v| {
+            *v = *v + 1;
+        });
+    inc_iter.iter((), &mut e);
+    let v_inc = evaluator.eval(&e);
     println!("result={}", v_inc);
 }
