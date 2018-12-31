77bb1c13e6374c85f188ea146636b71f02fcb548
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2018-12-31 11:45:05 +0300

    redo example

--- rustacata/src/main.rs
+++ rustacata/src/main.rs
@@ -1,90 +1,84 @@
 #![feature(proc_macro)]
 
 // Expression AST type
+
+enum Bop { Add, Sub, Mul, Div, }
+
 enum Expr {
     Value(i32),
-    Add(Box<Expr>, Box<Expr>),
-    Mult(Box<Expr>, Box<Expr>),
+    Binop(Bop, Box<Expr>, Box<Expr>),
 }
 
-trait Transformer<A, B> {
-    fn transform(&self, a: A) -> B;
-
-//    fn compose<Tr1, Tr2>(tr1: Tr1, tr2: Tr2) -> impl Tr3
-//        where
-//            Tr1: Transformer<A, B>,
-//            Tr2: Transformer<B, C>,
-//            Tr3: Transformer<A, C>;
+trait Gear<A, B> {
+    fn apply(&mut self, x: A) -> B;
 }
 
-//trait ExprAlg<A> {
-//    fn tr_value(&self, x: &i32) -> A;
-//    fn tr_add<F>(&self, fa: F, fb: F) -> A where F: FnOnce() -> A;
-//    fn tr_mult<F>(&self, fa: F, fb: F) -> A where F: FnOnce() -> A;
-//}
+mod expr {
+    use std::marker::PhantomData;
 
-struct ExprFold<A> {
-    fold_value : for<'b> fn(&'b i32) -> A,
-    fold_add : for <'b> fn(&'b dyn Fn() -> A, &'b dyn Fn() -> A) -> A,
-    fold_mult : for <'b> fn(&'b dyn Fn() -> A, &'b dyn Fn() -> A) -> A,
-}
+    pub trait Folder<A> {
+        fn value(&mut self, x: &i32) -> A;
+        fn binop(&mut self, op: &::Bop, a: A, b: A) -> A;
+    }
 
-impl<A> ExprFold<A> {
+    struct GearBox<A, F>
+        where F : Folder<A> {
+        folder : F,
+        pd : PhantomData<A>,
+    }
 
-    fn new() -> Self {
-        ExprFold {
-            fold_value: |&i| unimplemented!(),
-            fold_add: |e1, e2| unimplemented!(),
-            fold_mult: |e1, e2| unimplemented!(),
+    impl<A, F> ::Gear<&::Expr, A> for GearBox<A, F>
+        where F : Folder<A> {
+        fn apply(&mut self, x : &::Expr) -> A {
+            match *x {
+                ::Expr::Value(ref v) =>
+                    self.folder.value(v),
+                ::Expr::Binop(ref op, ref a, ref b) => {
+                    let a = self.apply(a);
+                    let b = self.apply(b);
+                    self.folder.binop(op, a, b)
+                }
+            }
         }
     }
 
-    fn with_fold_value(self, f: for<'b> fn(&'b i32) -> A) -> Self
-    {
-        ExprFold { fold_value: f, ..self }
-    }
-
-    fn with_fold_add(self, f: for <'b> fn(&'b dyn Fn() -> A, &'b dyn Fn() -> A) -> A) -> Self
-    {
-        ExprFold { fold_add: f, ..self }
-    }
-
-    fn with_fold_mult(self, f: for <'b> fn(&'b dyn Fn() -> A, &'b dyn Fn() -> A) -> A) -> Self
-    {
-        ExprFold { fold_mult: f, ..self }
+    pub fn gear<A, F>(folder : F) -> impl for<'a> ::Gear<&'a ::Expr, A>
+        where F : Folder<A> {
+        GearBox { folder, pd : PhantomData }
     }
 }
 
-impl<'b, A> Transformer<&'b Expr, A> for ExprFold<A> {
-    fn transform(&self, x: &'b Expr) -> A {
-        match *x {
-            Expr::Value(ref v) => (self.fold_value)(v),
-            Expr::Add(ref e1, ref e2) => (self.fold_add)(&|| self.transform(&*e1), &|| self.transform(&*e2)),
-            Expr::Mult(ref e1, ref e2) => (self.fold_mult)(&|| self.transform(&*e1), &|| self.transform(&*e2)),
+struct Evaluator();
+
+impl expr::Folder<i32> for Evaluator {
+
+    fn value(&mut self, x: &i32) -> i32 {
+        *x
+    }
+
+    fn binop(&mut self, op: &::Bop, a: i32, b: i32) -> i32 {
+        match op {
+            Bop::Add => a + b,
+            Bop::Sub => a - b,
+            Bop::Mul => a * b,
+            Bop::Div => a / b,
         }
     }
 }
 
 fn main() {
-    //    // 2 * (7 + 1) = 16
-    let e = Expr::Mult(
+    let mut eval = expr::gear(Evaluator());
+
+    // 2 * (7 + 1) = 16
+    let e = Expr::Binop(
+        Bop::Mul,
         Box::new(Expr::Value(2)),
-        Box::new(Expr::Add(
+        Box::new(Expr::Binop(
+            Bop::Add,
             Box::new(Expr::Value(7)),
             Box::new(Expr::Value(1))
         ))
     );
 
-    let evaluator = ExprFold::new()
-        .with_fold_value(|v| {
-            *v
-        })
-        .with_fold_add(|e1, e2| {
-            e1() + e2()
-        })
-        .with_fold_mult(|e1, e2| {
-            e1() * e2()
-        });
-
-    println!("result={}", evaluator.transform(&e));
+    println!("result: {}", eval.apply(&e));
 }
